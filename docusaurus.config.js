682bccff17aa1c26cecfaa19c4fcd35557e8a720
mohamadmoustafa <mohamad.moustafa@qmbase.com>
2023-01-16 11:20:23 +0100

    url edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://github.com/mohamadmoustafa/mytestrepo',
+  url: 'https://github.com/mohamadmoustafa/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
